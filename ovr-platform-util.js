6e22d8a876edc393ca4cf51a91b108da0af27ef6
Jonathan Perret <j-github@jonathanperret.net>
2021-11-06 10:12:41 +0100

    Use the Mac version of ovr-platform-util
    It seems Oculus recently changed the packaging method for
    the Windows executable of ovr-platform-util and the app's
    JavaScript code no longer appears in the clear in the binary.
    
    Fortunately, the latest Mac executable still works with our
    current method.

--- ovr-platform-util.js
+++ ovr-platform-util.js
@@ -1,9 +1,9 @@
 (function () {
     const fs = require('fs');
-    if (!fs.existsSync('ovr-platform-util.exe')) {
-        require('child_process').execFileSync('curl', ['-L', '-o', 'ovr-platform-util.exe', 'https://www.oculus.com/download_app/?id=1076686279105243']);
+    if (!fs.existsSync('ovr-platform-util')) {
+        require('child_process').execFileSync('curl', ['-L', '-o', 'ovr-platform-util', 'https://www.oculus.com/download_app/?id=1462426033810370']);
     }
-    const buf = fs.readFileSync('ovr-platform-util.exe');
+    const buf = fs.readFileSync('ovr-platform-util');
     const startIndex = buf.indexOf('var __BUNDLE_START_TIME__');
     const realStartIndex = buf.indexOf('\n', startIndex) + 1;
     const endIndex = buf.indexOf(0, realStartIndex);
